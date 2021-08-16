4cdcc2822c688e2b1dc37bd3d62b15fb2fe8e394
Lexa <ily4.farmer@yandex.ru>
2021-08-16 20:06:52 +0300

    исправлен баг

--- src/Components/ColoredUserpic/ColoredUserpic.js
+++ src/Components/ColoredUserpic/ColoredUserpic.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import "./ColoredUserpic.sass"
 
 const ColoredUserpic = ({src, size, margin, backgroundColor, colors, hoverColors, colorWidth}) => {
-    const [color, setColor] = useState(`linear-gradient(270deg, ${colors[0]} 30%, ${colors[1]}`),
-          colorOne = `linear-gradient(270deg, ${colors[0]} 30%, ${colors[1]}`,
-          colorTwo = `linear-gradient(270deg, ${hoverColors[0]} 30%, ${hoverColors[1]}`;
+    const [color, setColor] = useState(`linear-gradient(270deg, ${hoverColors[0]}, ${hoverColors[1]} 70%`),
+          colorOne = `linear-gradient(270deg, ${colors[0]} 20%, ${colors[1]}`,
+          colorTwo = `linear-gradient(270deg, ${hoverColors[0]}, ${hoverColors[1]} 70%`;
     return (
-        <div style={{backgroundColor: backgroundColor, height:size, width:size}} className="icon">
-            <div style={{padding: colorWidth, background: color}} 
+        <div style={{ height:size, width:size, padding: colorWidth, background: color}} 
                 onMouseEnter={() => setColor(colorOne)}
                 onMouseLeave={() => setColor(colorTwo)} 
+                className="icon">
+            <div style={{backgroundColor: backgroundColor}} 
+                
                 className="icon__outline">
                 <img style={{margin: margin}} className="icon__img" src={src} alt={src} />
             </div>
